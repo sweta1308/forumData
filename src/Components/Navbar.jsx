56b66bfb10c7cb9c2800f0e7f1d39a3776ee75e2
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-24 21:51:08 +0530

    feat: add nav data

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+  return (
+    <div
+      style={{
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+        padding: "5px 20px",
+        position: "sticky",
+        top: "0",
+        backgroundColor: "white",
+        zIndex: "2"
+      }}
+    >
+      <h1 style={{ paddingLeft: "70px" }}>forumData</h1>
+    </div>
+  );
+};
