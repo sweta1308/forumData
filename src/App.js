4181c16ecc410d0e5347f49c3a2693db18cbff0a
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-24 21:50:16 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import "./App.css";
+import { Feed } from "./pages/Feed/Feed";
+import { SinglePost } from "./pages/Singlepost/Singlepost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/singlepost/:id" element={<SinglePost />} />
+      </Routes>
     </div>
   );
 }
